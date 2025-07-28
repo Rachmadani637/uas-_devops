b359777ff7d86936e11554d356b3e5bffd9dd322
Rachmadani637 <rachmadani637@gmail.com>
2025-07-28 14:15:52 +0700

    Update app.py

--- tech-nova-inventory/order-service/app.py
+++ tech-nova-inventory/order-service/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Order Service Active"
+    try:
+        inventory_response = requests.get("https://inventory-service-m53o.onrender.com/")
+        return f"Order Service Active<br>Response from Inventory: {inventory_response.text}"
+    except Exception as e:
+        return f"Order Service Active<br>Failed to connect to Inventory Service: {e}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
