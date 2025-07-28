22afff8f896c447462768ed4c19d07675aedabf4
Rachmadani637 <rachmadani637@gmail.com>
2025-07-28 12:03:26 +0700

    Tambah route check-inventory ke inventory-service

--- tech-nova-inventory/auth-service/app.py
+++ tech-nova-inventory/auth-service/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Auth Service Active"
+
+@app.route("/check-inventory")
+def check_inventory():
+    try:
+        # Ganti dengan URL Render dari inventory-service kamu
+        inventory_url = "https://inventory-service-xxxxx.onrender.com"
+        response = requests.get(inventory_url)
+        return f"Response from Inventory Service: {response.text}"
+    except Exception as e:
+        return f"Error contacting Inventory Service: {str(e)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
